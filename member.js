8f7faa5cb22d54c8709894361c791c447c8df867
rajkumarinfomagnus <109267052+rajkumarinfomagnus@users.noreply.github.com>
2024-05-15 09:03:35 +0000

    added member.js

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "John";
+    member.age = 25;
+    member.skills = ["JavaScript", "HTML", "CSS"];
+    return member;
+}
